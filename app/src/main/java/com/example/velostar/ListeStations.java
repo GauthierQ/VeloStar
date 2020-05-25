178dade3984980d031bfddfdb1cba5c5d5167c08
Ibrahima Ndiaye <ndiayeibrahim666@gmail.com>
2020-05-25 15:52:07 +0200

    ajout classe ListeStations

---
+++ app/src/main/java/com/example/velostar/ListeStations.java
@@ -0,0 +1,47 @@
+package com.example.velostar;
+
+import java.util.ArrayList;
+
+public class ListeStations {
+
+        /**  Membres Privés */
+        private ArrayList<Station> _lesStations;	// pour contenir une liste de stations
+        private double _positionLatitude;	// latitude de la position de l'utilisateur (0 si inconnue)
+        private double _positionLongitude;	// longitude de la position de l'utilisateur (0 si inconnue)
+
+        /** Constructeurs */
+        public ListeStations()
+        {  // création de la liste
+            this._lesStations = new ArrayList<Station>();
+            _positionLatitude = 0;
+            _positionLongitude = 0;
+        }
+        public ListeStations(double unePositionLatitude, double unePositionLongitude)
+        {  // création de la liste
+            this._lesStations = new ArrayList<Station>();
+            _positionLatitude = unePositionLatitude;
+            _positionLongitude = unePositionLongitude;
+        }
+
+        /** Accesseurs */
+        public double getPositionLatitude() {
+            return _positionLatitude;
+        }
+        public void setPositionLatitude(double uneLatitude) {
+            _positionLatitude = uneLatitude;
+        }
+        public double getPositionLongitude() {
+            return _positionLongitude;
+        }
+        public void setPositionLongitude(double uneLongitude) {
+            _positionLongitude = uneLongitude;
+        }
+
+        /** Méthodes d'instance publiques */
+        public void ajouteStation(Station uneStation)
+        {  // ajoute l'objet à la liste
+            this._lesStations.add(uneStation);
+        }
+
+
+    }
