aee2c038e2aa6a3a9a1a7408e1946c7b73159799
GauthierQ <queau.gauthier@orange.fr>
2020-05-28 14:34:24 +0200

    affichage liste

--- app/src/main/java/com/example/velostar/retrofit/StationService.java
+++ app/src/main/java/com/example/velostar/retrofit/StationService.java
@@ -7,7 +7,15 @@ import retrofit2.http.GET;
 
 public interface StationService {
 
-    @GET("records/1.0/search/?dataset=vls-stations-etat-tr&q=&facet=nom&facet=etat&facet=nombreemplacementsactuels&facet=nombreemplacementsdisponibles&facet=nombrevelosdisponibles&facet=lastupdate&facet=idstation&facet=coordonnees")
+    @GET("records/1.0/search/?dataset=vls-stations-etat-tr&q=" +
+            "&facet=nom" +
+            "&facet=etat" +
+            "&facet=nombreemplacementsactuels" +
+            "&facet=nombreemplacementsdisponibles" +
+            "&facet=nombrevelosdisponibles" +
+            "&facet=lastupdate" +
+            "&facet=idstation" +
+            "&facet=coordonnees")
     Call<DataContainer> getDatas();
 
 }
