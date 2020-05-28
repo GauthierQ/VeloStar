985c323d5bbb9a6bd1820322b13546599110a74b
delasalle-sio-blandin-l <delasalle.sio.blandin.l@gmail.com>
2020-05-28 14:35:04 +0200

    Merge branch 'master' of C:\Users\Lucas\StudioProjects\VeloStar with conflicts.

--- app/src/main/java/com/example/velostar/retrofit/StationService.java
+++ app/src/main/java/com/example/velostar/retrofit/StationService.java
@@ -1,13 +1,11 @@
 package com.example.velostar.retrofit;
 
-import com.example.velostar.model.DataContainer;
-
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface StationService {
 
-    @GET("records/1.0/search/?dataset=vls-stations-etat-tr&q=&facet=nom&facet=etat&facet=nombreemplacementsactuels&facet=nombreemplacementsdisponibles&facet=nombrevelosdisponibles&facet=lastupdate&facet=idstation&facet=coordonnees")
-    Call<DataContainer> getDatas();
+    @GET("fields")
+    Call<DataContainer> getCards();
 
 }
